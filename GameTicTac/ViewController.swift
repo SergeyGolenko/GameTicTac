82e2c5d6ca4a84c1e6f7268e76d53e6861a6d3b2
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-23 17:16:39 +0200

    set buttons

--- GameTicTac/ViewController.swift
+++ GameTicTac/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var board: UIImageView!
+    
+    
+    @IBOutlet weak var buttonPlayerOutlet: UIButton!
+    
+    @IBAction func buttonPlayer(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

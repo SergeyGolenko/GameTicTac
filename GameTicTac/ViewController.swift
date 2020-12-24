8ea552ff982a79cd2d351a9b45a78b52f45368d5
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-24 11:31:06 +0200

    add function for checks winner position

--- GameTicTac/ViewController.swift
+++ GameTicTac/ViewController.swift
@@ -11,32 +11,40 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var board: UIImageView!
     
+    var activeGame = true
     var activePlayer = 1
     
     var gameState = [0,0,0,0,0,0,0,0,0] // 0 - Empty, 1 - Nought, 2 - Cross
+    let winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,8],[2,5,8],[0,4,8],[2,4,6]]
     
     
     
 
     
     @IBAction func buttonPlayer(_ sender: UIButton) {
+        var activePosition = sender.tag - 1
         
         
-        if gameState[(sender.tag) - 1] == 0 {
+        if gameState[(sender.tag) - 1] == 0 && activeGame {
         
         
         if activePlayer == 1 {
         sender.setImage(UIImage(named: "nought"), for:[])
-            gameState[sender.tag - 1] = 1
+            gameState[activePosition] = 1
             activePlayer = 2
             print(gameState)
         } else if activePlayer == 2 {
             sender.setImage(UIImage(named: "cross"), for:[])
-            gameState[sender.tag - 1] = 2
+            gameState[activePosition] = 2
             activePlayer = 1
-            print(gameState)
+              }
             
         }
+        for combination in winningCombinations{
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
+                print("WIN")
+                activeGame = false
+            }
             
         }
         
